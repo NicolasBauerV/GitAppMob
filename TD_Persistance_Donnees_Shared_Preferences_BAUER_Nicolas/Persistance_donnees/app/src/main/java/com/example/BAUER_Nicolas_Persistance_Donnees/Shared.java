710b870c56eed89dbee971b723d5920cd931483a
Nicolas_Bauer <bauer.nicolas266@gmail.com>
2021-06-08 16:39:56 +0200

    Apps Mobiles
    Shared prefs : Stockage interne, externe, fichier

---
+++ TD_Persistance_Donnees_Shared_Preferences_BAUER_Nicolas/Persistance_donnees/app/src/main/java/com/example/BAUER_Nicolas_Persistance_Donnees/Shared.java
@@ -0,0 +1,14 @@
+package com.example.BAUER_Nicolas_Persistance_Donnees;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Shared extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shared);
+    }
+}
